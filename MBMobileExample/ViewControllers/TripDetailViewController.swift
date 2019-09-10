b84a524d79bf54729c134a8558795d3d1bfd736a
lukasmohs <lukasmohs@gmail.com>
2019-09-10 20:49:18 +0200

    added first sample chart

--- MBMobileExample/ViewControllers/TripDetailViewController.swift
+++ MBMobileExample/ViewControllers/TripDetailViewController.swift
@@ -8,14 +8,25 @@
 
 import Foundation
 import UIKit
+import SwiftChart
 
 
 class TripDetailViewController: UIViewController {
     
     var trip: Trip?
 
+    @IBOutlet weak var chartView: Chart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
          self.navigationItem.title = "Trip Details"
+        
+        setUpChart()
+    }
+    
+    func setUpChart() {
+
+        let series = ChartSeries([0, 6.5, 2, 8, 4.1, 7, -3.1, 10, 8])
+        chartView.add(series)
     }
 }
