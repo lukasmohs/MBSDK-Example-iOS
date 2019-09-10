63e3fe860596a6f943f0e77d49c3a0c88d3cd251
lukasmohs <lukasmohs@gmail.com>
2019-09-11 01:26:14 +0200

    Added UIColor Green Codes

---
+++ MBMobileExample/Helper/UIColor+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extension.swift
+//  MBMobileExample
+//
+//  Created by Lukas Mohs on 11.09.19.
+//  Copyright © 2019 Daimler AG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    struct Ridebee {
+        static let BadGreen = UIColor(rgbaValue: 0xDCEDC8)
+        static let BetterGreen = UIColor(rgbaValue: 0x9CCC65)
+        static let OkGreen = UIColor(rgbaValue: 0x689F38)
+        static let BestGreen = UIColor(rgbaValue: 0x33691E)
+    }
+}
